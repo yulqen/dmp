307f20af978b51a5a7c3faf1609a4e9825cf3f63
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-05-29 17:27:37 +0100

    small refactor

--- test_dmp.py
+++ test_dmp.py
@@ -17,13 +17,17 @@ def test_reg_cycle_repr(cycle):
 
 
 def test_reg_cycle_working_days():
+    """
+    The list of tuples are month, day dates representing weekends,
+    which should not appear in the list of working days.
+    """
     years = {2022: [(1, 8), (1, 9), (5, 1)], 2023: [(1, 1), (2, 18), (9, 16)]}
     for y, v in years.items():
         cycle = RegulatoryCycle(y)
         we1 = date(y, v[0][0], v[0][1])
         we2 = date(y, v[1][0], v[1][1])
         we3 = date(y, v[2][0], v[2][1])
-        _working = cycle.base_working_days
-        assert we1 not in _working
-        assert we2 not in _working
-        assert we3 not in _working
+        working = cycle.base_working_days
+        assert we1 not in working
+        assert we2 not in working
+        assert we3 not in working
